5e39918b5f10ba6d98ce03cd37e022f720f4bf70
Hafiz Rizqi Hernanda <hafizhernanda5@gmail.com>
2025-09-22 10:42:40 +0700

    praktikum ke-2

--- dasarWeb/Pertemuan ke-4/tipe_data.php
+++ dasarWeb/Pertemuan ke-4/tipe_data.php
@@ -12,4 +12,18 @@ echo "Variable a: {$d} <br>";
 echo "Variable a: {$e} <br>";
 
 var_dump($e);
+echo "<br>";
+
+$nilaiMatematika = 5.1;
+$nilaiIPA = 6.7;
+$nilaiBahasaIndonesia = 9.3;
+
+$rataRata = ($nilaiMatematika + $nilaiIPA + $nilaiBahasaIndonesia) / 3;
+
+echo "Matematika: {$nilaiMatematika} <br>";
+echo "IPA: {$nilaiIPA} <br>";
+echo "Bahasa Indonesia: {$nilaiBahasaIndonesia} <br>";
+echo "Rata - rata: {$rataRata} <br>";
+
+var_dump($rataRata);
 ?>
