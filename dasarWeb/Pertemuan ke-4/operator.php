e34183506a725d75fd2dc41a42c1b9cf3c9e27df
Hafiz Rizqi Hernanda <hafizhernanda5@gmail.com>
2025-09-22 11:10:34 +0700

    praktikum ke-3

--- dasarWeb/Pertemuan ke-4/operator.php
+++ dasarWeb/Pertemuan ke-4/operator.php
@@ -9,10 +9,25 @@ $hasilBagi = $a / $b;
 $sisaBagi = $a % $b;
 $pangkat = $a ** $b;
 
+echo "Hasil Penjumlahan $a dan $b adalah $hasilTambah <br>";
+echo "Hasil Pengurangan $a dan $b adalah $hasilKurang <br>";
+echo "Hasil Perkalian $a dan $b adalah $hasilKali <br>";
+echo "Hasil Pembagian $a dan $b adalah $hasilBagi <br>";
+echo "Hasil Sisa bagi $a dan $b adalah $sisaBagi <br>";
+echo "Hasil Pangkat $a dan $b adalah $pangkat <br>";
+echo "<br>";
+
 $hasilSama = $a == $b;
 $hasilTidakSama = $a != $b;
-$hasilSama = $a < $b;
-$hasilSama = $a > $b;
-$hasilSama = $a <= $b;
-$hasilSama = $a >= $b;
+$hasilLebihKecil = $a < $b;
+$hasilLebihBesar = $a > $b;
+$hasilLebihKecilSama = $a <= $b;
+$hasilLebihBesarSama = $a >= $b;
+
+echo "Hasil Pembanding ' == ' $a dengan $b adalah $hasilSama <br>";
+echo "Hasil Pembanding ' != ' $a dengan $b adalah $hasilTidakSama <br>";
+echo "Hasil Pembanding ' < ' $a dengan $b adalah $hasilLebihKecil <br>";
+echo "Hasil Pembanding ' > ' $a dengan $b adalah $hasilLebihBesar <br>";
+echo "Hasil Pembanding ' <= ' $a dengan $b adalah $hasilLebihKecilSama <br>";
+echo "Hasil Pembanding ' >= ' $a dengan $b adalah $hasilLebihBesarSama <br>";
 ?>
