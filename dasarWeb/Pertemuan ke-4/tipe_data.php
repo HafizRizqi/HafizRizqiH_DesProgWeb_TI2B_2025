6000a236aca9ab58e1ae669225b41ac2acf71df9
Hafiz Rizqi Hernanda <hafizhernanda5@gmail.com>
2025-09-22 10:35:25 +0700

    praktikum ke-2

---
+++ dasarWeb/Pertemuan ke-4/tipe_data.php
@@ -0,0 +1,12 @@
+<?php
+$a = 10;
+$b = 5;
+$c = $a + 5;
+$d = $b + (10 * 5);
+$e = $d - $c;
+
+echo "Variable a: {$a} <br>";
+echo "Variable a: {$b} <br>";
+echo "Variable a: {$c} <br>";
+echo "Variable a: {$d} <br>";
+echo "Variable a: {$e} <br>";
