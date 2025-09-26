711c25e2ff04f75e7ea20bcdf96452b094bbaf65
Hafiz Rizqi Hernanda <hafizhernanda5@gmail.com>
2025-09-26 20:12:06 +0700

    praktikum ke-3

--- dasarWeb/Pertemuan ke-4/operator.php
+++ dasarWeb/Pertemuan ke-4/operator.php
@@ -24,12 +24,12 @@ $hasilLebihBesar = $a > $b;
 $hasilLebihKecilSama = $a <= $b;
 $hasilLebihBesarSama = $a >= $b;
 
-echo "Hasil Pembanding ' == ' $a dengan $b adalah $hasilSama <br>";
-echo "Hasil Pembanding ' != ' $a dengan $b adalah $hasilTidakSama <br>";
-echo "Hasil Pembanding ' < ' $a dengan $b adalah $hasilLebihKecil <br>";
-echo "Hasil Pembanding ' > ' $a dengan $b adalah $hasilLebihBesar <br>";
-echo "Hasil Pembanding ' <= ' $a dengan $b adalah $hasilLebihKecilSama <br>";
-echo "Hasil Pembanding ' >= ' $a dengan $b adalah $hasilLebihBesarSama <br>";
+echo "Hasil Pembanding ' == ' $a dengan $b adalah " . ($hasilSama ? "true" : "false") . "<br>";
+echo "Hasil Pembanding ' != ' $a dengan $b adalah " . ($hasilTidakSama ? "true" : "false") . "<br>";
+echo "Hasil Pembanding ' < ' $a dengan $b adalah " . ($hasilLebihKecil ? "true" : "false") . "<br>";
+echo "Hasil Pembanding ' > ' $a dengan $b adalah " . ($hasilLebihBesar ? "true" : "false") . "<br>";
+echo "Hasil Pembanding ' <= ' $a dengan $b adalah " . ($hasilLebihKecilSama ? "true" : "false") . "<br>";
+echo "Hasil Pembanding ' >= ' $a dengan $b adalah " . ($hasilLebihBesarSama ? "true" : "false") . "<br>";
 echo "<br>";
 
 $hasilAnd = $a && $b;
@@ -37,9 +37,36 @@ $hasilOr = $a || $b;
 $hasilNotA = !$a;
 $hasilNotB = !$b;
 
-echo "Hasil Logika '&&' $a dengan $b adalah $hasilAnd <br>";
-echo "Hasil Logika '||' $a dengan $b adalah $hasilOr <br>";
-echo "Hasil Logika '!' $a adalah $hasilNotA <br>";
-echo "Hasil Logika '!' $b adalah $hasilNotB <br>";
+echo "Hasil Logika '&&' $a dengan $b adalah " . ($hasilAnd ? "true" : "false") . "<br>";
+echo "Hasil Logika '||' $a dengan $b adalah " . ($hasilOr ? "true" : "false") . "<br>";
+echo "Hasil Logika '!' $a adalah " . ($hasilNotA ? "true" : "false") . "<br>";
+echo "Hasil Logika '!' $b adalah " . ($hasilNotB ? "true" : "false") . "<br>";
+echo "<br>";
+$hasilTambah = $a;
+$hasilTambah += $b;
 
+$hasilKurang = $a;
+$hasilKurang -= $b;
+
+$hasilKali = $a;
+$hasilKali *= $b;
+
+$hasilBagi = $a;
+$hasilBagi /= $b;
+
+$hasilMod = $a;
+$hasilMod %= $b;
+
+echo "Hasil Penugasan '+= ' $a dengan $b adalah $hasilTambah <br>";
+echo "Hasil Penugasan '-= ' $a dengan $b adalah $hasilKurang <br>";
+echo "Hasil Penugasan '*= ' $a dengan $b adalah $hasilKali <br>";
+echo "Hasil Penugasan '/= ' $a dengan $b adalah $hasilBagi <br>";
+echo "Hasil Penugasan '%= ' $a dengan $b adalah $hasilMod <br>";
+echo "<br>";
+
+$hasilIdentik = $a === $b;
+$hasilTidakIdentik = $a !== $b;
+
+echo "Hasil identik $a dengan $b adalah " . ($hasilIdentik ? "true" : "false") . "<br>";
+echo "Hasil Tidak identik $a dengan $b adalah " . ($hasilTidakIdentik ? "true" : "false") . "<br>";
 ?>
