ed98a132971e2baff2b75fcf5c6427667d7653bf
Hafiz Rizqi Hernanda <hafizhernanda5@gmail.com>
2025-09-29 10:42:27 +0700

    praktikum ke-7

---
+++ dasarWeb/Jobsheet05_PHP-2/string1.php
@@ -0,0 +1,13 @@
+<?php
+$loremIpsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+Voluptatem reprehenderit nobis veritatis commodi fugiat molestias
+impedit under ipsum voliptatum. corrupti minus sit excepturi nostrum
+quisquam? Quos impedit eum nulla optio. ";
+
+echo "<p>{$loremIpsum}</p>";
+echo "Panjang karakter: " . strlen($loremIpsum) . "<br>";
+echo "Panjang kata: " . str_word_count($loremIpsum) . "<br>";
+echo "<p>" . strtoupper($loremIpsum) . "</p>";
+echo "<p>" . strtolower($loremIpsum) . "</p>";
+
+?>
