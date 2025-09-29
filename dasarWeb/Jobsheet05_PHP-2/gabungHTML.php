537cee0f7080fa714ba792247f384f36f4cfa5d1
Hafiz Rizqi Hernanda <hafizhernanda5@gmail.com>
2025-09-29 10:53:58 +0700

    praktikum ke-8

---
+++ dasarWeb/Jobsheet05_PHP-2/gabungHTML.php
@@ -0,0 +1,9 @@
+<?php
+echo '<html>';
+echo '<head>';
+echo '<title>Cara 02</title>';
+echo '</head>';
+echo '<body>';
+echo '<p>Tanggal Hari ini : ' . date("d-m-Y") . '</p>';
+echo '</body>';
+echo '</html>';
