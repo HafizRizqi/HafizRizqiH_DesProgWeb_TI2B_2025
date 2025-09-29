24f25e9a7586031510ccc4f48fb86b86c5cc8c18
Hafiz Rizqi Hernanda <hafizhernanda5@gmail.com>
2025-09-29 09:53:27 +0700

    praktikum ke-4

---
+++ dasarWeb/Jobsheet05_PHP-2/function.php
@@ -0,0 +1,10 @@
+<?php
+
+function perkenalan() {
+   echo "Assalamu'alaikum, <br>";
+   echo "Perkenalkan, nama saya Elok. <br>";
+   echo "Senang berkenalan dengan Anda. <br>";
+}
+perkenalan();
+perkenalan();
+?>
