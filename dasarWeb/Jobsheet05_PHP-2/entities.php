34b843d350b93624ff4724f60912736110d4c3cc
Hafiz Rizqi Hernanda <hafizhernanda5@gmail.com>
2025-09-30 16:46:44 +0700

    Praktikum ke-9

---
+++ dasarWeb/Jobsheet05_PHP-2/entities.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Entities HTML</title>
+    </head>
+    <body>
+        <p>It&#39; time to read HTML5 book.</p>
+        <p>&nbsp;&nbsp;&nbsp;&nbsp;Keuntungan dari menggunakan nama entities:
+            Sebuah nama entitas muda diingat. Kerugian dari menggunakan nama
+            entities: Browser mungkin tidak mendukung semua nama entitas, tetapi
+            dukungan untuk nomor lebih baik.</p>
+            <p>&#169; 203 jti.com</p>
+</body>
+</html>
+        
