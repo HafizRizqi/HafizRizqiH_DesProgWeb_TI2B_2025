a4c557aa01b969ac8119d18a00be0a9187f3dbe4
Hafiz Rizqi Hernanda <hafizhernanda5@gmail.com>
2025-10-06 09:38:01 +0700

    praktikum ke-1

--- dasarWeb/Pertemuan ke-7/isset.php
+++ dasarWeb/Pertemuan ke-7/isset.php
@@ -2,7 +2,15 @@
 
 $umur;
 if (isset($umur) && $umur >= 18) {
-    echo "Anda Sudah dewasa";
+    echo "Anda Sudah dewasa <br>";
 } else {
-    echo "Anda belum dewasa atau variabel 'umur' belum di temukan";
+    echo "Anda belum dewasa atau variabel 'umur' belum di temukan <br>";
 }
+
+$data = array("nama" => "Jane", "umur" => 25);
+if (isset($data["nama"])) {
+    echo "<br>Nama : " . $data["nama"];
+} else {
+    echo "<br> Variabel 'nama' tidak ditemukan dalam array.";
+}
+?>
