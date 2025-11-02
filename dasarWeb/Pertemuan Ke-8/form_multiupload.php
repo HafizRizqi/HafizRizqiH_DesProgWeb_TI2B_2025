941188d82bf12c59c468eb9202ac4bd1c3a476f4
Hafiz Rizqi Hernanda <hafizhernanda5@gmail.com>
2025-11-02 22:40:49 +0700

    Praktikum 2 Soal 9

--- dasarWeb/Pertemuan Ke-8/form_multiupload.php
+++ dasarWeb/Pertemuan Ke-8/form_multiupload.php
@@ -5,7 +5,7 @@
     <body>
         <h2>Unggah Dokumen</h2>
         <form action="proses_upload.php" method="post" enctype="multipart/form-data">
-            <input type="file" name="file[]" multiple ="multiple" accept=".pdf, .doc, .docx">
+            <input type="file" name="file[]" multiple ="multiple" accept=".jpg, .png, .jpeg">
             <input type="submit" value="Unggah">
         </form>
     </body>
