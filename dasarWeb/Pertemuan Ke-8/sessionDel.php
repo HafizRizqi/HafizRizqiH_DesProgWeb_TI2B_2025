475a515836cd2176722b662cdb2ece14d88f09f6
Hafiz Rizqi Hernanda <hafizhernanda5@gmail.com>
2025-11-02 23:40:03 +0700

    Praktikum 9

---
+++ dasarWeb/Pertemuan Ke-8/sessionDel.php
@@ -0,0 +1,15 @@
+<?php 
+session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+    <body>
+        <?php 
+            session_unset(); // Menghapus semua variabel session
+            session_destroy(); // Menghancurkan session
+            
+            echo "All session variables are now removed, and the session is destroyed.";
+        ?>
+    </body>
+</html>
