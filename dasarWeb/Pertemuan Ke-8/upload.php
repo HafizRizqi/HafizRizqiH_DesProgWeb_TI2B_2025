2e83e1f7b6e268610575fc3efe69df9e4ef1b6ff
Hafiz Rizqi Hernanda <hafizhernanda5@gmail.com>
2025-11-02 22:06:50 +0700

    Praktikum 1 Langkah 6 dan Menjawab Soal 5

--- dasarWeb/Pertemuan Ke-8/upload.php
+++ dasarWeb/Pertemuan Ke-8/upload.php
@@ -2,12 +2,20 @@
 if (isset($_POST["submit"])) {
     $targetdir = "uploads/"; //Direktori tujuan untuk menyimpan file 
     $targetfile = $targetdir . basename($_FILES["myfile"]["name"]);
+    $fileType = strtolower(pathinfo($targetfile, PATHINFO_EXTENSION));
 
-    if (move_uploaded_file($_FILES["myfile"]["tmp_name"], $targetfile)) {
-        echo "File berhasil diunggah.";
+    $allowedExtensions = array("jpg", "jpeg", "png", "gif");
+    $maxsize = 5 * 1024 * 1024; // 5MB
+
+    if (in_array($fileType, $allowedExtensions) && $_FILES["myfile"]["size"] <= $maxsize) {
+        if (move_uploaded_file($_FILES["myfile"]["tmp_name"], $targetfile)) {
+        echo "File berhasil diunggah.<br>";
+        echo "<img src='$targetfile' width='200' alt='Thumbnail Gambar'>";
     } else {
         echo "Gagal mengunggah file.";
     }
+    } else {
+        echo "File tidak valid atau melebihi ukuran maksimum yang diizinkan.";
+    }
 }
-
 ?>
