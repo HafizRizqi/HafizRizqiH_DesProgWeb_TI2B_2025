21f843c4875f008cb94f1cda1f6404e388846ce1
Hafiz Rizqi Hernanda <hafizhernanda5@gmail.com>
2025-12-28 21:23:21 +0700

    Praktikum ke-3 : Folder Template

---
+++ dasarWeb/Pertemuan ke-11/admin/template/footer.php
@@ -0,0 +1,8 @@
+<script src="assets/dist/js/bootstrap.bundle.min.js"></script>
+
+<script src="https://cdn.jsdelivr.net/npm/chart.js@4.3.2/dist/chart.umd.js" integrity="sha384-eITPs3rJLXLlSHh3JdIISYN/nJoSsrbKrhCTrr2xt+EMPSMOVBXb1L6SGGz0Lp" crossorigin="anonymous"></script>
+
+<script src="assets/custom/dashboard.js"></script>
+</body>
+
+</html>
