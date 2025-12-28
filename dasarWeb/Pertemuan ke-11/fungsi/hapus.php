3afae6f3432fcd7cbe5c9f569ac49f42401e3f86
Hafiz Rizqi Hernanda <hafizhernanda5@gmail.com>
2025-12-28 22:13:13 +0700

    Praktikum ke-5 : Folder Module – Bagian Anggota

--- dasarWeb/Pertemuan ke-11/fungsi/hapus.php
+++ dasarWeb/Pertemuan ke-11/fungsi/hapus.php
@@ -13,6 +13,28 @@ if (!empty($_SESSION['username'])) {
             pesan('danger', "Jabatan Tidak Terhapus Karena: " . mysqli_error($koneksi));
         }
         header("Location: ../index.php?page=jabatan");
+    } elseif (!empty($_GET['anggota'])) {
+    $id = antiinjection($koneksi, $_GET['id']);
+
+    // 1. Hapus data anggota terlebih dahulu
+    $query1 = "DELETE FROM anggota WHERE user_id = '$id'";
+    if (mysqli_query($koneksi, $query1)) {
+
+        // 2. Hapus data user
+        $query2 = "DELETE FROM user WHERE id = '$id'";
+        if (mysqli_query($koneksi, $query2)) {
+            pesan('success', "Anggota berhasil dihapus.");
+        } else {
+            pesan('warning', "Data anggota terhapus, tetapi data user gagal dihapus: " . mysqli_error($koneksi));
+        }
+
+    } else {
+        pesan('danger', "Data anggota gagal dihapus: " . mysqli_error($koneksi));
     }
+
+    header("Location: ../index.php?page=anggota");
+}
+
+
 }
 ?>
